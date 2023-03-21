41a1a937400620c501464866e2865537a0646c38
Alois Napitalai <anapitalai@gmail.com>
2023-03-21 17:57:26 +1000

    SCP action works

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React - Authentic</h1>
+      <h1>Authentic Kid Skills</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Vite looks cools and easy
+       Project for kids
       </p>
     </div>
   )
